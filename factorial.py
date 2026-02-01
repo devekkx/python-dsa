9ff406b98124f861c9d63cbb8628067548bfb261
Dev Ekkx <devekkx@gmail.com>
2026-02-01 20:20:46 +0000

    feature: implement factorial

---
+++ factorial.py
@@ -0,0 +1,18 @@
+import math
+
+# def num_possible_orders(num_posts):
+#    p = 1
+#    for i in range(1, num_posts + 1):
+#        p *= i
+#    return p
+
+
+# def num_possible_orders(num_posts):
+#     if num_posts <= 1:
+#         return num_posts
+#     return num_posts * num_possible_orders(num_posts-1)
+
+# def num_possible_orders(num_posts):
+#     return math.factorial(num_posts)
+
+# print(num_possible_orders(5))
