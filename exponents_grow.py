6a8e3de3f3d464d69da9a75de0285c4abd815f53
Dev Ekkx <devekkx@gmail.com>
2026-02-15 11:37:00 +0000

    feature: implement nested loops

--- exponents_grow.py
+++ exponents_grow.py
@@ -1,14 +1,34 @@
-def get_follower_prediction(follower_count, influencer_type, num_months):
-    factor = 2
-    match influencer_type:
-        case "fitness":
-            factor = 4
-        case "cosmetic":
-            factor = 3
-    return follower_count * (factor ** num_months)
+# def get_follower_prediction(follower_count, influencer_type, num_months):
+#     factor = 2
+#     match influencer_type:
+#         case "fitness":
+#             factor = 4
+#         case "cosmetic":
+#             factor = 3
+#     return follower_count * (factor ** num_months)
 
-print(get_follower_prediction(
-    follower_count= 10,
-    influencer_type= "fitness",
-    num_months=1
-))
+# print(get_follower_prediction(
+#     follower_count= 10,
+#     influencer_type= "fitness",
+#     num_months=1
+# ))
+
+
+def get_avg_brand_followers(all_handlers, brand_name):
+    count = 0
+    for handles in all_handlers:
+        for handle in handles:
+            if brand_name in handle:
+                count += 1
+    avg = count / len(all_handlers)
+    return avg
+
+
+
+all_handles=[
+    ["cosmos232", "cosmoer"],[ "lady443", "cosmofather"]
+]
+
+brand_name = "cosmo"
+
+print(get_avg_brand_followers(all_handles, brand_name))
