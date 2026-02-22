11c8f06a268c0ca8568d06629aeecb1d9d73ccba
Dev Ekkx <devekkx@gmail.com>
2026-02-22 10:50:10 +0000

    feature: implement factorial calculation using built in method

--- factorial.py
+++ factorial.py
@@ -1,18 +1,20 @@
 import math
 
 # def num_possible_orders(num_posts):
-#    p = 1
-#    for i in range(1, num_posts + 1):
-#        p *= i
-#    return p
+#     p = 1
+#     for i in range(1, num_posts + 1):
+#         p *= i
+#     return p
 
 
 # def num_possible_orders(num_posts):
 #     if num_posts <= 1:
 #         return num_posts
-#     return num_posts * num_possible_orders(num_posts-1)
+#     return num_posts * num_possible_orders(num_posts - 1)
 
-# def num_possible_orders(num_posts):
-#     return math.factorial(num_posts)
 
-# print(num_possible_orders(5))
+def num_possible_orders(num_posts):
+    return math.factorial(num_posts)
+
+
+print(num_possible_orders(5))
